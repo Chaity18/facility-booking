6e1b75540535c9ca59aea397db7e6091b1f6a710
chaitya <cychaitya18082000@gmail.com>
2022-05-11 22:33:15 +0530

    fix some changes in controller

--- src/Controller/BookingCheckController.php
+++ src/Controller/BookingCheckController.php
@@ -29,31 +29,21 @@ class BookingCheckController extends AbstractController
 
     public function calculateAndBook(object $booking)
     {
-        $userBookings = $this->getBookings();
-        foreach($userBookings as $usersBooking) {
-            if($booking->startTime == $usersBooking->startTime && $booking->endTime == $usersBooking->endTime && $usersBooking->id == $booking->id) {
-                $booking->total = null;
-                $booking->booked = false;  
-            }else{
-                $perHourCharge = 0;
-                if($booking->service == 'club_house') {
-                    $perHourCharge = 100;
-                    $endTime = explode(' ',$booking->endTime)[1];
-                    if(strtotime($endTime) >= strtotime('16:00:00')) {
-                        $perHourCharge = 500; 
-                    } 
-                }elseif ($booking->service == 'tenis_court') {
-                        $perHourCharge = 50;
-                }
-        
-                $timeDifference = $this->getTimeDifference(new DateTime($booking->startTime), new DateTime($booking->endTime));
-                $total = $timeDifference * $perHourCharge;
-                $booking->total = $total;
-                $booking->booked = true;
-            }
+        $perHourCharge = 0;
+        if($booking->service == 'club_house') {
+            $perHourCharge = 100;
+            $endTime = explode(' ',$booking->endTime)[1];
+            if(strtotime($endTime) >= strtotime('16:00:00')) {
+                $perHourCharge = 500; 
+            } 
+        }elseif ($booking->service == 'tenis_court') {
+                $perHourCharge = 50;
         }
 
-        return $booking;
+        $timeDifference = $this->getTimeDifference(new DateTime($booking->startTime), new DateTime($booking->endTime));
+        $total = $timeDifference * $perHourCharge;
+
+        $this->findByService($booking->service,$booking, $total);
     }
     
     public function getTimeDifference(DateTime $startDateTime, DateTime $endDateTime) 
@@ -61,6 +51,42 @@ class BookingCheckController extends AbstractController
         $dateDiff = $startDateTime->diff($endDateTime);
         return $dateDiff->h;
     } 
+
+    public function findByService($service, $book, $total) {
+        $allBokings = $this->getBookings();
+        $searchRes = [];
+        
+        foreach ($allBokings as $booking) {
+            if($booking->service == $service) {
+                array_push($searchRes, $booking);
+            }
+        }
+        
+        $resOne = (array) $searchRes[0];
+        
+        if(count($searchRes) > 1){
+            $resTwo = (array) $searchRes[1];
+            $sameData = array_intersect_assoc($resOne, $resTwo);
+            if(in_array($book->service,$sameData) && in_array($book->startTime,$sameData) && in_array($book->endTime,$sameData)) {
+                if($searchRes[0]->id == $book->id) {
+                    $book->total = $total;
+                    $book->booked = true;
+                } else {
+                    $book->total = 0;
+                    $book->booked = false;
+                }
+            } else {
+                $book->total = $total;
+                $book->booked = true;
+            }
+        } else {
+            $book->total = $total;
+            $book->booked = true;
+        }
+        
+        return $book;
+
+    }
 
     public function getBookings()
     {
@@ -82,6 +108,18 @@ class BookingCheckController extends AbstractController
         $bookingThree->startTime = '2021-10-26 16:00:00';
         $bookingThree->endTime = '2021-10-26 22:00:00';
 
-        return array($bookingOne, $bookingTwo, $bookingThree);
+        $bookingFour = new stdClass();
+        $bookingFour->id = 4;
+        $bookingFour->service = 'tenis_court';
+        $bookingFour->startTime = '2021-10-26 10:00:00';
+        $bookingFour->endTime = '2021-10-26 20:00:00';
+
+        $bookingFive = new stdClass();
+        $bookingFive->id = 5;
+        $bookingFive->service = 'club_house';
+        $bookingFive->startTime = '2021-10-26 22:00:00';
+        $bookingFive->endTime = '2021-10-26 24:00:00';
+
+        return array($bookingOne, $bookingTwo, $bookingThree, $bookingFour, $bookingFive);
     }
 }
